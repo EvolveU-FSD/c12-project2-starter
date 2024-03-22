6c90d533b1669924b2525ee6c83282aef25e7fe7
Chris Desmarais <desmarais_chris@hotmail.com>
2024-03-21 19:40:22 -0600

    Start of user data

---
+++ User.js
@@ -0,0 +1,6 @@
+var users = [ { name: 'Tony', team: 'Oilers'},
+            {name: 'Chris', team: 'Flames'} ]
+
+export function getUsers() {
+    return users;
+}
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-const express = require('express')
+import * as UserData from './User.js'
+import express from 'express'
+
 const app = express()
 
 app.use(express.json())
 app.use(express.static('public'))
 
-let user = { name: 'Tony', team: '501s'}
-
-app.get('/user', (req, res) => {
-    res.send(user)
+app.get('/users', (req, res) => {
+    res.send(UserData.getUsers())
 })
 
-app.post('/user', (req, res) => {
-    user = req.body
-    console.log('Got user', user)
-    res.sendStatus(200)
-})
+// app.get('/user', (req, res) => {
+//     res.send(user)
+// })
 
-app.delete('/user', (req, res) => {
-    user = {}
-    console.log('Deleting user!')
-    res.sendStatus(200)
-})
+// app.post('/user', (req, res) => {
+//     user = req.body
+//     console.log('Got user', user)
+//     res.sendStatus(200)
+// })
+
+// app.delete('/user', (req, res) => {
+//     user = {}
+//     console.log('Deleting user!')
+//     res.sendStatus(200)
+// })
 
 console.log('Listening on port 3000')
 app.listen(3000)
