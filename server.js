fb6c7bafdef4bbe48ce29081ef2beb0aa0451071
Tony Enerson <tony@enerson.ca>
2024-03-25 15:08:33 -0600

    Clean up comments

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-//const express = require('express')
 import express from 'express'
 import * as UserData from './users.js'
 const app = express()
 
 app.use(express.json());
-// app.use(express.static('public'))
-
-// let user = { name: 'Tony', team: '501s'}
+app.use(express.static('public'))
 
 app.get('/users', (req, res)=>{
     const users = UserData.getAllUsers();
@@ -30,26 +27,6 @@ app.post('/users', (req, res)=>{
     UserData.addUser(body)
     res.status(200).send('Ok');
 })
-
-// app.get('/user', (req, res) => {
-//     res.send(user)
-// })
-
-// app.get('/chris', (req,  res)=>{
-//     res.send('Chris says hi')
-// })
-
-// app.post('/user', (req, res) => {
-//     user = req.body
-//     console.log('Got user', user)
-//     res.sendStatus(200)
-// })
-
-// app.delete('/user', (req, res) => {
-//     user = {}
-//     console.log('Deleting user!')
-//     res.sendStatus(200)
-// })
 
 console.log('Listening on port 3000')
 app.listen(3000)
