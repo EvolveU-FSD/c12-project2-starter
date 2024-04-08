49ad1d51639cb0c2e9c1b116a81cc31e6e4d1ca9
Tony Enerson <tony@enerson.ca>
2024-04-07 21:06:29 -0600

    Quick Readme

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import usersController from './users/usersController.js'
 
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json());
 app.use('/api/user', usersController)
 
-app.listen(process.env.PORT || 3000)
+app.listen(PORT, () => {
+    console.log('Server running on localhost ' + PORT)
+})
