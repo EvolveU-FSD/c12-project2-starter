2c5ce0b3f6c44a256c6e2520a8cb8130efd6d1ee
Tony Enerson <tony@enerson.ca>
2024-04-08 11:14:46 -0600

    Rendering names

--- client/src/pages/AllUsersPage.jsx
+++ client/src/pages/AllUsersPage.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
+import { getAllUsers } from "../api"
 
 function AllUsersPage() {
+    const [allUsers, setAllUsers] = useState([])
+    const [loadError, setLoadError] = useState()
+
+    async function loadUsers() {
+        try {
+            const users = await getAllUsers()
+            setAllUsers(users)    
+        }
+        catch (error) {
+            setLoadError(error)
+        }
+    }    
+
+    useEffect(() => {
+        loadUsers()
+    }, [])
+
     return (
-        <h1>All Users</h1>
+        <div>
+            <h1>All Users</h1>
+            { loadError && <div>Error: { loadError.message }</div> }
+            { allUsers.map((user) => (
+                <div key={user._id}> { user.name }</div>
+            ))}
+            <div>User count: { allUsers.length }</div>
+        </div>
     )
 }
 
